1474916ac195fab1233b32a746133a60363e3f40
maximmanah12345 <55143520+maximmanah12345@users.noreply.github.com>
2019-09-17 16:45:15 +0300

    Меню

--- main.cpp
+++ main.cpp
@@ -2,16 +2,30 @@
 
 #include "TXLib.h"
 
+void drawButton(int x, int y, HDC baton, const char* text)
+{
+    txSelectFont("Arial", 30);
+    txTransparentBlt(txDC(), x,   y, 530, 140, baton, 0,0 ,TX_WHITE);
+    txDrawText      (x, y, x + 530, y + 140, text);
+}
+
 int main()
     {
     txCreateWindow(1200, 700);
-    HDC  poni = txLoadImage ("poni.bmp");
+    //HDC  poni = txLoadImage ("poni.bmp");
+    HDC  baton = txLoadImage ("Menu_Button.bmp");
+    drawButton(0, 0, baton,   "онмх"  ) ;
+    drawButton(0, 130, baton, "убняр" ) ;
+    drawButton(0, 260, baton,"йношрю" ) ;
+    drawButton(0, 390, baton,"цнкнбю" ) ;
+    drawButton(0, 520, baton, "рекн"  ) ;
 
-    txTransparentBlt(txDC(), 400, 250, 600, 400, poni, 0,0 ,TX_WHITE);
 
 
 
-    txDeleteDC (poni);
+
+    //txDeleteDC (poni);
+    txDeleteDC (baton);
 
     return 0;
     }
